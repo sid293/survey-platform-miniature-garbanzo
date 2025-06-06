830ea162928495fe9f1c7e94c250868b56b20080
sid293 <siddheshgawade61@gmail.com>
2025-06-06 20:11:28 +0530

    cors setting

--- survey-platform-backend/app/index.js
+++ survey-platform-backend/app/index.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [
+        process.env.FRONTEND_URL || 'http://localhost:3000',
+        "https://survey-platform-miniature-garbanzo-theta.vercel.app"
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
